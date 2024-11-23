ad85685530c172c210c7b228b92816ca74c24ee5
kamja0510 <kamja990510@gmail.com>
2024-11-24 04:39:29 +0900

    [FEAT] #18 OtherPingUiState 생성

---
+++ app/src/main/java/org/sopt/and4ever/presentation/otherping/OtherPingUiState.kt
@@ -0,0 +1,7 @@
+package org.sopt.and4ever.presentation.otherping
+
+import org.sopt.and4ever.domain.model.MyPing
+
+data class OtherPingUiState(
+    val pingList: List<MyPing>
+)
--- app/src/main/java/org/sopt/and4ever/presentation/otherping/OtherPingViewModel.kt
+++ app/src/main/java/org/sopt/and4ever/presentation/otherping/OtherPingViewModel.kt
@@ -2,12 +2,25 @@ package org.sopt.and4ever.presentation.otherping
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
 import org.sopt.and4ever.data.service.OtherPingService
 
 class OtherPingViewModel(
     private val otherPingService: OtherPingService
 ): ViewModel() {
 
+    fun getOtherPingList(){
+        viewModelScope.launch{
+            runCatching {
+                otherPingService.getOtherPingList()
+            }.onSuccess {
+
+            }.onFailure {
+
+            }
+        }
+    }
 }
 
 class OtherPingViewModelFactory(private val otherPingService: OtherPingService) :
