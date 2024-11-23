50eb59fdadf25666629d8e3d3445832bac7735d9
kamja0510 <kamja990510@gmail.com>
2024-11-24 05:39:09 +0900

    [FEAT] #22 커스텀 MyPingDetailSnackBar 구현

---
+++ app/src/main/java/org/sopt/and4ever/presentation/mypingdetail/MyPingDetailSnackBar.kt
@@ -0,0 +1,62 @@
+package org.sopt.and4ever.presentation.mypingdetail
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import org.sopt.and4ever.R
+import org.sopt.and4ever.core.theme.G07
+import org.sopt.and4ever.core.theme.Head06
+
+@Composable
+fun MyPingDetailSnackBar(
+    message: String
+){
+    Box(
+        modifier = Modifier
+            .border(
+                width = 6.dp,
+                color = Color(0xFFFF4A63),
+                shape = RoundedCornerShape(12.dp)
+            )
+    ){
+        Row(
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Image(
+                painter = painterResource(R.drawable.img_eyes),
+                contentDescription = ""
+            )
+
+            Spacer(modifier = Modifier.width(18.dp))
+
+            Text(
+                text = "완벽한 ",
+                color = Color(0xFFFF4A63),
+                style = Head06
+            )
+            Text(
+                text = message,
+                color = G07,
+                style = Head06
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun MyPingDetailSnackBarPreview(){
+    MyPingDetailSnackBar("")
+}
