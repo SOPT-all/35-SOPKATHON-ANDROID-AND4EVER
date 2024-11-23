6d8e894d3f37227683b7ee89441c8e4dbfe79aa3
Thirfir <sangbbang31@gmail.com>
2024-11-23 21:30:22 +0900

    [FEAT] #3 HomeScreen 추가

---
+++ app/src/main/java/org/sopt/and4ever/presentation/home/HomeScreen.kt
@@ -0,0 +1,13 @@
+package org.sopt.and4ever.presentation.home
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+
+@Composable
+fun HomeScreen(
+    modifier: Modifier = Modifier,
+    viewModel: HomeViewModel = viewModel()
+) {
+
+}
--- app/src/main/java/org/sopt/and4ever/presentation/main/JPNavigation.kt
+++ app/src/main/java/org/sopt/and4ever/presentation/main/JPNavigation.kt
@@ -1,5 +1,6 @@
 package org.sopt.and4ever.presentation.main
 
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
@@ -7,6 +8,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import org.sopt.and4ever.core.navigation.Route
+import org.sopt.and4ever.presentation.home.HomeScreen
 
 @Composable
 fun JPNavigation(
@@ -19,7 +21,9 @@ fun JPNavigation(
         startDestination = Route.Home
     ) {
         composable<Route.Home> {
-
+            HomeScreen(
+                modifier = Modifier.fillMaxWidth()
+            )
         }
 
         composable<Route.Input> {
