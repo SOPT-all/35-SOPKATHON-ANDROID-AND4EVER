cd7698af82461e951d9e5a0a58a7f0c151e96cec
kamja0510 <kamja990510@gmail.com>
2024-11-24 07:47:20 +0900

    [FEAT] #22 custom snackbar 구현 완료

--- app/src/main/java/org/sopt/and4ever/presentation/mypingdetail/MyPingDetailSnackBar.kt
+++ app/src/main/java/org/sopt/and4ever/presentation/mypingdetail/MyPingDetailSnackBar.kt
@@ -1,15 +1,19 @@
 package org.sopt.and4ever.presentation.mypingdetail
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -20,16 +24,20 @@ import org.sopt.and4ever.core.theme.G07
 import org.sopt.and4ever.core.theme.Head06
 
 @Composable
-fun MyPingDetailSnackBar(
-    message: String
-){
+fun MyPingDetailSnackBar(){
     Box(
         modifier = Modifier
             .border(
                 width = 6.dp,
                 color = Color(0xFFFF4A63),
                 shape = RoundedCornerShape(12.dp)
+            ).fillMaxWidth()
+            .background(
+                color = Color.White,
+                shape = RoundedCornerShape(12.dp)
             )
+            .padding(vertical = 15.dp),
+        contentAlignment = Alignment.Center
     ){
         Row(
             horizontalArrangement = Arrangement.Center
@@ -47,7 +55,7 @@ fun MyPingDetailSnackBar(
                 style = Head06
             )
             Text(
-                text = message,
+                text = "핑계였군요!",
                 color = G07,
                 style = Head06
             )
@@ -58,5 +66,5 @@ fun MyPingDetailSnackBar(
 @Preview
 @Composable
 fun MyPingDetailSnackBarPreview(){
-    MyPingDetailSnackBar("")
+    MyPingDetailSnackBar()
 }
