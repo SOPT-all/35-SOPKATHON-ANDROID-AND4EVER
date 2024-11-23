e3e3a7839bbbcceaf5bb7cad111907af860b8578
kamja0510 <kamja990510@gmail.com>
2024-11-24 03:35:31 +0900

    [FEAT] #18 OtherPingViewModelFactory 구현

--- app/src/main/java/org/sopt/and4ever/presentation/otherping/OtherPingViewModel.kt
+++ app/src/main/java/org/sopt/and4ever/presentation/otherping/OtherPingViewModel.kt
@@ -1,7 +1,23 @@
 package org.sopt.and4ever.presentation.otherping
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import org.sopt.and4ever.data.service.MyPingService
+import org.sopt.and4ever.data.service.OtherPingService
+import org.sopt.and4ever.presentation.myping.MyPingViewModel
 
-class OtherPingViewModel: ViewModel() {
+class OtherPingViewModel(
+    private val otherPingService: OtherPingService
+): ViewModel() {
 
+}
+
+class OtherPingViewModelFactory(private val otherPingService: OtherPingService) :
+    ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(OtherPingViewModel::class.java)) {
+            return OtherPingViewModel(otherPingService) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
 }
