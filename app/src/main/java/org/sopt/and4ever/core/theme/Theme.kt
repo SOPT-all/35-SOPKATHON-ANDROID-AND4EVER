a76304130544fd28b5cdac1b1b8cf667acd7a7c1
kamja0510 <kamja990510@gmail.com>
2024-11-23 22:56:33 +0900

    [ADD] #5 글꼴 시스템 추가

--- app/src/main/java/org/sopt/and4ever/core/theme/Theme.kt
+++ app/src/main/java/org/sopt/and4ever/core/theme/Theme.kt
@@ -8,6 +8,7 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
@@ -48,10 +49,17 @@ fun AND4EVERTheme(
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
+    CompositionLocalProvider(LocalJPTypography provides Typography) {
+        MaterialTheme(
+            colorScheme = colorScheme,
+            typography = androidx.compose.material3.Typography(),
+            content = content
+        )
+    }
+}
 
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
-        content = content
-    )
+object JPTheme {
+    val typography: JPTypography
+        @Composable
+        get() = LocalJPTypography.current
 }
--- app/src/main/java/org/sopt/and4ever/core/theme/Type.kt
+++ app/src/main/java/org/sopt/and4ever/core/theme/Type.kt
@@ -1,34 +1,119 @@
 package org.sopt.and4ever.core.theme
 
-import androidx.compose.material3.Typography
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.sp
+import org.sopt.and4ever.R
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
+
+val pretendardFontFamily = FontFamily(
+    Font(resId = R.font.pretendard_bold, weight = FontWeight.Bold),
+    Font(resId = R.font.pretendard_regular, weight = FontWeight.Normal)
+)
+
+internal val Typography = JPTypography(
+    head06 = TextStyle(
+        fontFamily = pretendardFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp,
+        lineHeight = TextUnit.Unspecified,
         letterSpacing = 0.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+    head05 = TextStyle(
+        fontFamily = pretendardFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 18.sp,
+        lineHeight = TextUnit.Unspecified,
+        letterSpacing = 0.sp
+    ),
+    head04 = TextStyle(
+        fontFamily = pretendardFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 16.sp,
+        lineHeight = TextUnit.Unspecified,
+        letterSpacing = 0.sp
+    ),
+    head03 = TextStyle(
+        fontFamily = pretendardFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp,
+        lineHeight = TextUnit.Unspecified,
+        letterSpacing = 0.sp
+    ),
+    head02 = TextStyle(
+        fontFamily = pretendardFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 12.sp,
+        lineHeight = TextUnit.Unspecified,
+        letterSpacing = 0.sp
+    ),
+    head01 = TextStyle(
+        fontFamily = pretendardFontFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 10.sp,
+        lineHeight = TextUnit.Unspecified,
+        letterSpacing = 0.sp
+    ),
+    body04 = TextStyle(
+        fontFamily = pretendardFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 15.sp,
+        lineHeight = TextUnit.Unspecified,
+        letterSpacing = 0.sp
+    ),
+    body03 = TextStyle(
+        fontFamily = pretendardFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        lineHeight = TextUnit.Unspecified,
+        letterSpacing = 0.sp
+    ),
+    body02 = TextStyle(
+        fontFamily = pretendardFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
+        lineHeight = TextUnit.Unspecified,
+        letterSpacing = 0.sp
+    ),
+    body01 = TextStyle(
+        fontFamily = pretendardFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 10.sp,
+        lineHeight = TextUnit.Unspecified,
+        letterSpacing = 0.sp
     )
-    */
 )
+
+@Immutable
+data class JPTypography(
+    val head06: TextStyle,
+    val head05: TextStyle,
+    val head04: TextStyle,
+    val head03: TextStyle,
+    val head02: TextStyle,
+    val head01: TextStyle,
+    val body04: TextStyle,
+    val body03: TextStyle,
+    val body02: TextStyle,
+    val body01: TextStyle
+)
+
+val LocalJPTypography = staticCompositionLocalOf {
+    JPTypography(
+        head06 = TextStyle.Default,
+        head05 = TextStyle.Default,
+        head04 = TextStyle.Default,
+        head03 = TextStyle.Default,
+        head02 = TextStyle.Default,
+        head01 = TextStyle.Default,
+        body04 = TextStyle.Default,
+        body03 = TextStyle.Default,
+        body02 = TextStyle.Default,
+        body01 = TextStyle.Default
+    )
+}
